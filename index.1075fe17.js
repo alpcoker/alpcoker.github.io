30b8af3fc2b438557b09695744843e7c2d5e73d7
alpcoker <alpcoker@users.noreply.github.com>
2022-01-16 18:48:50 +0000

    deploy: 46f99f7f2a6e58f02bc88b455fdd48e95ef07f92

---
+++ index.1075fe17.js
@@ -0,0 +1,2 @@
+setTimeout((function(){const s=document.body;s.className=s.className.replace(/\bis-loading\b/,"is-playing"),setTimeout((function(){s.className=s.className.replace(/\bis-playing\b/,"is-ready")}),2750)}),100);
+//# sourceMappingURL=index.1075fe17.js.map
