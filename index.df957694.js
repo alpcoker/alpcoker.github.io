1a1daf322655e14b806cf06dda61333befa18de6
alpcoker <alpcoker@users.noreply.github.com>
2022-06-26 15:51:37 +0000

    deploy: 087981e0f1f8b825d745addfe026550ba9a9187b

--- index.df957694.js
+++
@@ -1,2 +0,0 @@
-setTimeout((function(){var a=document.body;a.className=a.className.replace(/\bis-loading\b/,"is-playing"),setTimeout((function(){a.className=a.className.replace(/\bis-playing\b/,"is-ready")}),2750)}),100);
-//# sourceMappingURL=index.df957694.js.map
